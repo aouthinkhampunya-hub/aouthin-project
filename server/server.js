29e8a3cb09068a103b9330accc8c3e21448df74c
aouthin <aouthinkhampunya@gmail.com>
2026-08-31 08:51:06 +0700

    ອແກ້ໄຊພອດ deploy

--- server/server.js
+++ server/server.js
@@ -9,8 +9,7 @@ const authRouter = require('./routes/auth');
 const requireAuth = require('./middleware/requireAuth');
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
